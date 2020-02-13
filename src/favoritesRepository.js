344d439a3c147e9e053332576a15c5495360a39d
Erik Hoffman <erik.johansson87@gmail.com>
2020-02-13 10:38:35 +0100

    generation of key pattern as well

--- src/favoritesRepository.js
+++ src/favoritesRepository.js
@@ -1,7 +1,6 @@
 const redisClient = require("./helpers/redisClient");
 
 const KEY_PREFIX = "favorites";
-const KEY_USER = userId => `*${KEY_PREFIX}:${userId}*`;
 const generateKey = (...args) => args.join(":");
 const ONE_YEAR = 1 * 60 * 60 * 24 * 365;
 
@@ -14,7 +13,8 @@ const store = async (userId, assetId) => {
 
 const get = async userId => {
   if (!userId) return false;
-  const keys = await redisClient.keys(KEY_USER(userId));
+  const pattern = generateKey(`*${KEY_PREFIX}`, `${userId}*`);
+  const keys = await redisClient.keys(pattern);
   if (!keys) return [];
 
   const requestPromises = [];
